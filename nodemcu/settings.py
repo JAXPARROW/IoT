ff51a224d94beb4ff73a4e7f86a39de9bbb674e6
Jackson Linus <jacksonlinus95@gmail.com>
2021-02-15 00:04:09 +0300

    added cache into db

--- nodemcu/settings.py
+++ nodemcu/settings.py
@@ -88,6 +88,13 @@ DATABASES = {
         }
     }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'iot_cache',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
