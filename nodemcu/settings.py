6814213f58a7912d796fea7959e303e290e0c9d6
Jackson Linus <jacksonlinus95@gmail.com>
2021-02-15 00:12:06 +0300

    caching middleware

--- nodemcu/settings.py
+++ nodemcu/settings.py
@@ -41,6 +41,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    #caching middleware
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'nodemcu.urls'
