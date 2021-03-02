6406517b6778e3f6a047baebdd10c27debb245cf
Jackson Linus <jacksonlinus95@gmail.com>
2021-03-02 20:29:25 +0300

    Updated devices admin register

--- iot/admin.py
+++ iot/admin.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 from django.contrib import messages
 from django.utils.translation import ngettext
 
-from iot.models import Switch, Weather, Device
+from iot.models import Switch, UpdatedDevice, Weather, Device, FotaUpdate
 
 
+@admin.register(UpdatedDevice)
+class UpdatedDevice(admin.ModelAdmin):
+    list_display = ['device_id', 'hardware_id','type','version', 'updated_on']
+    list_filter = ['device_id','hardware_id','type','version']
+    search_fields = ['device_id','hardware_id','type','version']
+    list_per_page = 15
+
+
+@admin.register(FotaUpdate)
+class FotaUpdate(admin.ModelAdmin):
+    list_display = ['device_id','type','version','host','bin',]
+    list_editable = ['version',]
 
 
 
@@ -15,11 +27,7 @@ switch_off.short_description = "Switch OFF the devices"
 
 @admin.register(Device)
 class DeviceId(admin.ModelAdmin):
-    list_display = [
-        'device_id',
-        'owner',
-        'created_on',
-    ]
+    list_display = ['device_id','owner','created_on',]
     
 
 
@@ -45,29 +53,11 @@ class SwitchAdmin(admin.ModelAdmin):
 
     actions = [Switch_ON,Switch_OFF,]
 
-    list_display = [
-        'device_id',
-        'port_number',
-        'location',
-        'status'
-    ]
-    list_display_links = [
-        'device_id',
-        'location',
-    
-    ]
-    list_editable = [
-        'status',
-    ]
-    list_filter = [
-        'status',
-        'device_id',
-    ]
-    search_fields = [
-        'location',
-        'device_id',
-        'status',
-    ]
+    list_display = ['device_id','port_number','location','status']
+    list_display_links = ['device_id','location',]
+    list_editable = ['status',]
+    list_filter = ['status','device_id',]
+    search_fields = ['location','device_id','status',]
     
     def switch_On(self, request, queryset):
         update = queryset.update(status='ON')
@@ -78,15 +68,9 @@ class SwitchAdmin(admin.ModelAdmin):
             ) % update, messages.SUCCESS)
 
 
-
-
 @admin.register(Weather)
 class WeatherAdmin(admin.ModelAdmin):
-    list_display = [
-        'device_id',
-        'temperature',
-        'humidity'
-    ]
+    list_display = ['device_id','temperature','humidity']
 
 
 admin.site.site_header = "IoT"
